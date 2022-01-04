1b6fc3a82dd529010bfeaa02a05a172c600f60d3
jayanta-hub <jayanta.garu@gmail.com>
2022-01-04 11:51:00 +0530

    initisl commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import RegistrationForm from './Component/RegistrationForm'
+
+function App() {
+
+  return (
+    <div className="App">
+      <RegistrationForm/>
+      
+    </div>
+  );
+}
+
+export default App;
